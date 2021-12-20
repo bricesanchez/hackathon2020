a8659fd7360e945fdb06863e80467547f49e7422
dbil25 <dbil25@hotmail.com>
2021-12-20 15:37:12 -0500

    graph

--- app/components/state_temperature_graph_component.rb
+++ app/components/state_temperature_graph_component.rb
@@ -9,12 +9,18 @@ class StateTemperatureGraphComponent < ViewComponentReflex::Component
     set_data
   end
 
-  def increment
-    @count += 1
+  def change_filters(from_date: nil, to_date: nil, countries: nil, state: nil, from_temp: nil, to_temp: nil)
+    @from_date = from_date
+    @to_date = to_date
+    @countries = countries
+    @state = state
+    @from_temp = from_temp
+    @to_temp = to_temp
+    set_data
   end
 
   def set_data
-    data = StateTemperature
+    data = StateTemperature.where.not(averageTemperature: nil)
     if @from_date.present? && @to_date.present?
       data = data.where(date: @from_date..@to_date)
     end
@@ -27,7 +33,7 @@ class StateTemperatureGraphComponent < ViewComponentReflex::Component
     if @from_temp.present? && @to_temp.present?
       data = data.where(temp: @from_temp..@to_temp)
     end
-    @data = data
+    @data = data.where(country: "Canada").limit(100).group_by(&:country).transform_values { |temps| temps.map { |temp| {x: temp.recordedAt, y: temp.averageTemperature} } }
   end
 
   def self.stimulus_controller
