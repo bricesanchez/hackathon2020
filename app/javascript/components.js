02c5c604612bc988d19d7ee1c657dd4a6bd1a6fa
dbil25 <dbil25@hotmail.com>
2021-12-20 11:16:58 -0500

    try sidecar

--- app/javascript/components.js
+++ app/javascript/components.js
@@ -2,4 +2,4 @@ function importAll(r) {
   r.keys().forEach(r)
 }
 
-importAll(require.context("../components", true, /[_\/]component\.js$/))
+importAll(require.context("../components", true, /[_\/]component_controller\.js$/))
