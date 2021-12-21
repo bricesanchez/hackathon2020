2df901a2a910cdc7192fd3173dc72530535f20c0
Brice Sanchez <bricesanchez@users.noreply.github.com>
2021-12-21 17:45:35 -0500

    Improve UI

--- app/controllers/trump_tweets_controller.rb
+++ app/controllers/trump_tweets_controller.rb
@@ -1,8 +1,6 @@
 class TrumpTweetsController < ApplicationController
   def index
     @trump_tweet_columns = [:id, :content, :link]
-    # @trump_tweets = TrumpTweet.first(10).as_json(only: @trump_tweet_columns)
-
     @trump_tweets = TrumpTweet.order(created_at: :desc)
     @trump_tweets = @trump_tweets.search(params[:query]) if params[:query].present?
     @pagy, @trump_tweets = pagy @trump_tweets.reorder(sort_column => sort_direction), items: params.fetch(:count, 10)
